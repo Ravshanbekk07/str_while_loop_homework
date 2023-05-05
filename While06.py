749cba8dbc1f0a406dab039b8b40a212390538a3
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-05 19:07:32 +0500

    made

--- While06.py
+++ While06.py
@@ -7,3 +7,16 @@ def main(s):
     Returns:
         int: return answer
     """
+    i = 0
+    m = 0
+    while i<len(s):
+        if not s[i] =='o' and not s[i] =='e':
+           if not s[i] =='u' and not s[i] =='a':
+            if not s[i] =='i' and not s[i] =='y':
+              m+=1
+        
+        i+=1
+    return m
+
+v = main("CodeschoolUz")
+print(v)
