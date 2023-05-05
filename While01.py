3d78f2f8e07589e5620d4d1fbf79df18b3bde446
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-05 16:05:59 +0500

    made

--- While01.py
+++ While01.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    x = 0
+    while i < len(s):
+        if s[i].isdigit():
+          x+=1
+        i+=1  
+        
+    return x
+
+v = main("e324xe")
+print(v)
