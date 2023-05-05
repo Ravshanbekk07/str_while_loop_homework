1befe8ea502555f83182fc03590583dea8ec041e
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-05 18:52:08 +0500

    made

--- While05.py
+++ While05.py
@@ -6,4 +6,16 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    m = 0
+    while i<len(s):
+        if not s[i] =='o' and not s[i] =='e':
+           if not s[i] =='u' and not s[i] =='a':
+            if not s[i] =='i' and not s[i] =='y':
+              m+=1
+        
+        i+=1
+    return m
+
+v = main("aaaaad")
+print(v)
